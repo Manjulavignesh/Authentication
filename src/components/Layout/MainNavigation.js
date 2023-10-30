f52353c9f41f2a8f1f8c4de23a5c268ceb3f74c5
Manjulavignesh <manjulamuthu29792@gmail.com>
2023-10-30 20:13:54 +0530

    Logoutfunctionality

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import classes from './MainNavigation.module.css';
 import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
   const authCtx=useContext(AuthContext)
+  const history=useHistory();
+  const logoutHandler=()=>{
+    authCtx.logout();
+    history.replace("/auth")
+  }
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -20,7 +26,7 @@ const MainNavigation = () => {
             <Link to='/profile'>Profile</Link>
           </li>}
           {authCtx.isLoggedIn && <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>}
           
         </ul>
